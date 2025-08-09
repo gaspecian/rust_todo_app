a37acf185352fc54d1b979ee0e741e1715b9316e
Gabriel M. Specian <ga.specian@gmail.com>
2025-08-09 20:57:29 -0300

    Minor

--- src/swagger/doc_config.rs
+++ src/swagger/doc_config.rs
@@ -13,7 +13,6 @@ use crate::modules::health::{
 ///
 /// This struct defines the `OpenAPI` specification for the entire application,
 /// including all endpoints, schemas, and metadata.
-#[allow(clippy::needless_collect)]
 #[derive(OpenApi)]
 #[openapi(
     info(
